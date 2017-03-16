7981a9206a2fafd0b98a7781846b482bd200f6c5
Kendrick Tan <kendricktan0814@gmail.com>
2017-03-16 14:59:44 -0700

    shell and python scripts

--- setup_python.sh
+++ setup_python.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 sudo apt-get update
+sudo apt-get install fluidsynth
 sudo apt-get install python3 python3-dev python3-pip
 
 # Installing Conda
 if [[ ! -e "conda_install.sh" ]];
 then
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda_install.sh
-	sudo chmod +x conda_install.sh
-    sudo bash conda_install.sh
+	chmod +x conda_install.sh
+    bash conda_install.sh
 fi
+source ~/.zshrc
+source ~/.bashrc
 
-# Create
 conda create -n cuda python=3.6 anaconda numpy scipy matplotlib
-conda install -c soumith pytorch=0.1.10
+source activate cuda
+
+conda install pytorch torchvision cuda80 -c soumith
+pip install tqdm librosa pretty_midi
+
